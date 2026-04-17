6d75abc9824b780b341c8b98d25e7772c89183dd
Youssef BOUGHANMI <93406263+YoussefBOUGHANMI@users.noreply.github.com>
2026-04-17 03:13:19 +0200

    Update test.py

--- app/test.py
+++ app/test.py
@@ -2,5 +2,6 @@ import streamlit as st
 from challenge import *
 from accueil import *
 from tp import *
-pg = st.navigation([Accueil, TP , Challenge])
+#pg = st.navigation([Accueil, TP , Challenge])
+#pg = st.navigation([Accueil, TP , st.write("Surprise !!!! ")])
 pg.run()
